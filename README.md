a9b10a8cd5f357c8b0ebb779ae955796e6111b89
Viraj <vnavkal0@gmail.com>
2018-05-02 14:46:02 -0700

    restructure repository

---
+++ README.md
@@ -0,0 +1,3 @@
+## Unsupervised keypoint detection of goats
+
+This is a very experimental project to try to detect physically meaningful keypoints of the goats being cared for by the San Bruno 4-H club.  It is still a work in progress.
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='goats',
+    version='0.0',
+    description='Unsupervised keypoint detection of goats',
+    author='Viraj Navkal',
+    author_email='vnavkal0@gmail.com',
+    url='https://www.github.com/vnavkal/goats/',
+    packages=setuptools.find_packages()
+)
